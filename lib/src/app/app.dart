843c00860617e21e4b3510ed16df0fbdd727dd3f
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-07 07:48:11 +0200

    packages added

---
+++ lib/src/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HEKKKKKKKKKKKKKKKKKKKKo'),
+      ),
+      body: Column(
+        children: [
+          Text('HEllo '),
+        ],
+      ),
+    );
+  }
+}
