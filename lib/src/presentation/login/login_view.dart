6ef26db6977c88f1b7217c72812d7de91f474e17
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-09 13:52:18 +0200

    login screen added

--- lib/src/presentation/login/login_view.dart
+++ lib/src/presentation/login/login_view.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:foodapp/src/presentation/resources/colors_manger.dart';
+import 'package:foodapp/src/presentation/resources/font_manger.dart';
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
   @override
@@ -7,72 +9,44 @@ class LoginView extends StatelessWidget {
     return Scaffold(
       backgroundColor: ColorsManger.primaryColor,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              width: 300,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: ColorsManger.whiteColor,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30,
               ),
-              child: TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration:InputDecoration(
-                  hintStyle:TextStyle(
-                    color: ColorsManger.primaryColor,
-                  ),
-                  labelText: 'Email',
+              Container(
+                width: 300,
+                height: 60,
 
-                  icon:Padding(
-                    padding: const EdgeInsets.only(
-                      top:12,
-                      left: 12,
-                      bottom: 12,
-                      right: 12,
-                    ),
-                    child: Icon(Icons.email , color: ColorsManger.primaryColor,),
-                  ),
-                  border: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  errorBorder: InputBorder.none,
-                  disabledBorder: InputBorder.none,
-                  contentPadding:
-                  EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
-
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: ColorsManger.whiteColor,
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              width: 300,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: ColorsManger.whiteColor,
-              ),
-              child: TextFormField(
-                keyboardType: TextInputType.visiblePassword,
-                decoration:InputDecoration(
-                  labelText: 'Password',
-                 hintStyle: TextStyle( color: ColorsManger.primaryColor,),
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
 
+                  decoration:InputDecoration(
+                    labelStyle:TextStyle(
+                        color:ColorsManger.primaryColor
+                    ) ,
 
-                  icon:Padding(
+                    hintStyle:TextStyle(
+                      color: ColorsManger.primaryColor,
+                    ),
+                    labelText: 'Email',
+
+                    icon:Padding(
                       padding: const EdgeInsets.only(
                         top:12,
                         left: 12,
                         bottom: 12,
                         right: 12,
-
                       ),
-                      child: Icon(Icons.email , color:ColorsManger.primaryColor),
+                      child: Icon(Icons.email , color: ColorsManger.primaryColor,),
                     ),
                     border: InputBorder.none,
                     focusedBorder: InputBorder.none,
@@ -81,33 +55,99 @@ class LoginView extends StatelessWidget {
                     disabledBorder: InputBorder.none,
                     contentPadding:
                     EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
-                ) ,
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-
-            Container(
-              width: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: ColorsManger.whiteColor,
-              ),
-              child: FlatButton(
-                onPressed: () {
-                },
-                child: Text('Log in ',
-                  style: TextStyle(
-                    color: ColorsManger.primaryColor,
-                    fontSize: 25,
 
                   ),
                 ),
               ),
-            ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: 300,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: ColorsManger.whiteColor,
+                ),
+                child: TextFormField(
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration:InputDecoration(
+                    labelText: 'Password',
+                   hintStyle: TextStyle( color: ColorsManger.primaryColor,),
+                    icon:Padding(
+                        padding: const EdgeInsets.only(
+                          top:12,
+                          left: 12,
+                          bottom: 12,
+                          right: 12,
 
-          ],
+                        ),
+                        child: Icon(Icons.email , color:ColorsManger.primaryColor),
+                      ),
+                      labelStyle:TextStyle(
+                        color:ColorsManger.primaryColor
+                      ) ,
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      contentPadding:
+                      EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+                  ) ,
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+
+              Container(
+                width: 180,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: ColorsManger.whiteColor,
+                ),
+                child: FlatButton(
+                  onPressed: () {
+                  },
+                  child: Center(
+                    child: Text('Log in ',
+                      style: TextStyle(
+                        color: ColorsManger.primaryColor,
+                        fontSize: 20,
+
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Not a registerd user ? ',
+                      style: TextStyle(
+                        color: ColorsManger.whiteColor,
+                        fontSize: 16
+                      ),
+                    ),
+                    Text('Sign Up here ',
+                      style: TextStyle(
+                        color: ColorsManger.whiteColor,
+                        fontSize: 14,
+                        fontFamily: 'Montserrat-Black'
+                      ),
+                    ),
+
+                  ],
+                ),
+              ) ,
+            ],
+          ),
         ),
       ),
     );
