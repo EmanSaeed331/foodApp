3caca2ccd4c973710cf42ecaf2519bb5f78180b2
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-09 16:27:58 +0200

    home view added

--- lib/src/presentation/home/home_view.dart
+++ lib/src/presentation/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:foodapp/src/presentation/home/curved_nav_bar.dart';
 class HomeView extends StatelessWidget {
@@ -6,9 +7,55 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child:Text('Home View '
-      ),
+      backgroundColor: Colors.grey.shade200,
+      body: ListView.separated(
+          itemBuilder: (context,index) =>_itemBuilder(),
+          separatorBuilder: (context , index) => SizedBox(height: 0),
+          itemCount: 8),
+
+    );
+  }
+
+  Widget _itemBuilder(){
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              left: 10,
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                    backgroundImage:AssetImage('assets/images/food2.jpg'),
+                ),
+                  SizedBox(width: 20,),
+                Text('Recipe '),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+
+              child: Container(
+
+                height: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Image.asset('assets/images/food.jpg',
+                  fit:BoxFit.fitHeight,
+
+                ),
+
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
