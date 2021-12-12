f5d25a93126f23d73d92b941fe9627a737b56311
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-13 00:17:16 +0200

    profile design added

--- lib/src/presentation/profile/profile_view.dart
+++ lib/src/presentation/profile/profile_view.dart
@@ -1,11 +1,113 @@
 import 'package:flutter/material.dart';
 class ProfileView extends StatelessWidget {
-  const ProfileView({Key? key}) : super(key: key);
+  //const ProfileView({Key? key}) : super(key: key);
+  List images = [
+    'assets/images/food.jpg',
+    'assets/images/food.jpg',
+    'assets/images/food.jpg',
+    'assets/images/food.jpg',
+    'assets/images/food.jpg',
 
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Profile View')),
+      body: Column(
+
+
+          children: [
+        Padding(
+        padding: const EdgeInsets.only(
+            top: 100
+        ),
+        child: Center(
+          child: CircleAvatar(
+            radius: 50.0,
+            backgroundImage:AssetImage('assets/images/food.jpg'),
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 30,
+      ),
+      Text('Eman',
+        style: TextStyle(
+          fontFamily: 'Montserrat-Black',
+          fontSize: 20,
+        ),
+      ),
+
+          Container(
+            //child: griddesign(),
+          ),
+        ],
+      ),
     );
   }
+  Widget griddesign (){
+    return GridView.builder(
+          gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 2/3,
+            mainAxisSpacing: 2.3,
+            crossAxisSpacing: 2.3,),
+          itemCount: images.length,
+          itemBuilder: (BuildContext ctx, index) {
+            return Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                alignment: Alignment.center,
+                child: Image.asset('assets/images/food.jpg')
+
+              ),
+            );
+          });
+  }
 }
+/*
+* Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 100
+            ),
+            child: Center(
+              child: CircleAvatar(
+                radius: 50.0,
+                backgroundImage:AssetImage('assets/images/food.jpg'),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text('Eman',
+            style: TextStyle(
+              fontFamily: 'Montserrat-Black',
+              fontSize: 20,
+            ),
+          ),
+
+          griddesign(),
+        // Container(
+        //   child: GridView.builder(
+        //       itemCount: 5 ,
+        //       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //         crossAxisCount: 3,
+        //         crossAxisSpacing: 2.0,
+        //         mainAxisSpacing: 8.0 ,
+        //       ),
+        //       itemBuilder: (BuildContext context , index ){
+        //         return Text('Hello');
+        //
+        //       }),
+        // ),
+
+
+        ],
+
+      ),*/
